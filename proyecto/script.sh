1cd59fcb6032de2cfaefa1085431f70bf37c4f37
Lissandrodev <Luismexico2006@gmail.com>
2025-08-15 13:56:36 -0700

    Comandos basicos de bash

--- proyecto/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
- 
-echo "¿Como te llamas?"; read nombre
-echo "hola, $nombre!"
---
+++ variables/es.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "El Script usa" $# "parametros" 
+echo "Los tres primeros son: " $1, $2, $3
+
